23879e5b33755edb7e7256502f839bca95ffc3a0
Luigi Eliabe <luigi.eliabe.as@gmail.com>
2023-11-15 09:52:16 -0300

    Add role package

---
+++ src/main/java/com/edu/ifnmg/role/Role.java
@@ -0,0 +1,15 @@
+package com.edu.ifnmg.role;
+
+import com.edu.ifnmg.entity.Entity;
+
+public class Role extends Entity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
