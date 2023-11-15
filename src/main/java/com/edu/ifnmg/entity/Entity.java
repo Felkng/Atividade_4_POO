7f33e0a57b4aede50e77b55321a8a7ad70157323
Felkng <105019177+Felkng@users.noreply.github.com>
2023-11-15 08:24:04 -0300

    Create repository

---
+++ src/main/java/com/edu/ifnmg/entity/Entity.java
@@ -0,0 +1,20 @@
+package com.edu.ifnmg.entity;
+
+public class Entity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Entity(Long id) {
+        this.id = id;
+    }
+
+    public Entity(){}
+}
