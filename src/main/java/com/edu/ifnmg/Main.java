a92fa4f1f0ddefa6e04731755a3e43d18d8f33a2
Felkng <105019177+Felkng@users.noreply.github.com>
2023-11-17 23:07:56 -0300

    Remove teste G

--- src/main/java/com/edu/ifnmg/Main.java
+++ src/main/java/com/edu/ifnmg/Main.java
@@ -2,12 +2,12 @@ package com.edu.ifnmg;
 
 public class Main {
     public static void main(String[] args) {
-//        Tests.TestA();
-//        Tests.TestB();
-//        Tests.TestC();
-//        Tests.TestD();
-//        Tests.TestE();
-//        Tests.TestF();
-        Tests.TestG();
+         Tests.TestA();
+         Tests.TestB();
+         Tests.TestC();
+         Tests.TestD();
+         Tests.TestE();
+         Tests.TestF();
+
     }
 }
